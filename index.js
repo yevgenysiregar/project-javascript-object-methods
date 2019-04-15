e2a37c4adbc06e70366cd6afd382fbe392d5bc28
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-15 15:44:08 +0700

    Add code on index.js to try using object methods

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ const human = {
 };
 human.run();
 human.speed(44.16);
+
+const name = {};
+
+name["Egy"] = true;
+console.log(name["Egy"]); // console.log(name[Egy]); --> Results will be error
+
+const user = "Egy";
+name[user] = true;
+console.log(name["Egy"]); // console.log(name[Egy]); --> Results will be error
+
+const userInfo = {
+  name: "Yevgeny",
+  age: 20
+};
+const info = prompt("Which one do you want to know? Name or Age?");
+
+console.log(userInfo[info]); //if user type "name" = Yevgeny, if user type "age" = 20
+
+const fruit = prompt("Which fruit do you want to buy?");
+const bag = {
+  [fruit]: prompt("How many would you like?")
+};
+console.log(bag);
+
+const runnerName = function(name, age) {
+  console.log(name), console.log(age);
+};
+runnerName("Bolt", 32);
